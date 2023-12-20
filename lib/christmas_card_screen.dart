b11d2c49fa34114ae173c109ef759847a36c2cea
Ayaan <onestar7984@gmail.com>
2023-12-20 23:00:34 +0900

    refactor: ChristmasCardScreen 배경 그림 추가 Version 2

--- lib/christmas_card_screen.dart
+++ lib/christmas_card_screen.dart
@@ -5,42 +5,51 @@ class ChristmasCardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Happy',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 40),
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                  'https://images.unsplash.com/photo-1576919228236-a097c32a5cd4?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
             ),
-            SizedBox(
-              height: 8,
-            ),
-            Text(
-              'Christmas',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 40),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: [
-                Expanded(
-                    child: Text(
-                  'from. IL Seong',
-                  textAlign: TextAlign.end,
-                  style: TextStyle(fontSize: 24),
-                )),
-                SizedBox(
-                  width: 16,
-                ),
-              ],
-            ),
-          ],
+          ),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Happy',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 40),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                'Christmas',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 40),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    'from. IL Seong',
+                    textAlign: TextAlign.end,
+                    style: TextStyle(fontSize: 24),
+                  )),
+                  SizedBox(
+                    width: 16,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
