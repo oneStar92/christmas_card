2d790ac93484444d2614ea7ab9ec7dfcda72617c
Ayaan <onestar7984@gmail.com>
2023-12-20 22:52:46 +0900

    feat: ChristmasCardScreen 간단한 UI Version 1

--- lib/christmas_card_screen.dart
+++ lib/christmas_card_screen.dart
@@ -5,6 +5,44 @@ class ChristmasCardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Happy',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 40),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              'Christmas',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 40),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: Text(
+                  'from. IL Seong',
+                  textAlign: TextAlign.end,
+                  style: TextStyle(fontSize: 24),
+                )),
+                SizedBox(
+                  width: 16,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
